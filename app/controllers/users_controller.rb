4319272f85f37276bf9222e342061ef34afc5bac
Michelle Ranly <meranly8@gmail.com>
2020-12-15 22:09:52 -0600

    Created post users/login routeand began authenticate logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,13 @@ class UsersController < ApplicationController
         erb :"users/login"
     end
 
+    post '/users/login' do
+        @user = User.find_by(email: params[:user][:email])
+        
+        if @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect "/crops"
+        end
+    end
+
 end
