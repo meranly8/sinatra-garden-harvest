0f88f74a4ab5e55d72f19bf54333b4f1ccaf2723
Michelle Ranly <meranly8@gmail.com>
2020-12-17 14:37:24 -0600

    Created private Crops #set_crop to call in show, edit, update, delete routes to find  the crop specified in params

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -22,12 +22,12 @@ class CropsController < ApplicationController
     end
 
     get '/crops/:id' do
-        @crop = Crop.find_by(id: params[:id])
+        set_crop
         erb :"crops/show"
     end
 
     get '/crops/:id/edit' do
-        @crop = Crop.find_by(id: params[:id])
+        set_crop
         if @crop.user.id == current_user.id
             erb :"crops/edit"
         else
@@ -36,7 +36,7 @@ class CropsController < ApplicationController
     end
 
     patch '/crops/:id' do
-        @crop = Crop.find_by(id: params[:id])
+        set_crop
         
         if params[:crop].values.any? {|value| value.blank?}
             redirect "/crops/#{@crop.id}/edit"
@@ -48,13 +48,19 @@ class CropsController < ApplicationController
     end
 
     delete '/crops/:id' do
-        @crop = Crop.find_by(id: params[:id])
+        set_crop
         if @crop.user.id == current_user.id
             @crop.delete
             redirect "/users/#{current_user.id}"
         else
             redirect "/users/#{current_user.id}"
         end 
+    end
+
+    private #only to be used in this class
+
+    def set_crop
+        @crop = Crop.find_by(id: params[:id])
     end
 
 end
