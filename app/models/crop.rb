e8bd2114fabab97910a5e29b63cc6529073aef30
Michelle Ranly <meranly8@gmail.com>
2020-12-18 00:05:52 -0600

    Added validations into Crop model

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -1,4 +1,9 @@
 class Crop < ActiveRecord::Base
-    belongs_to :user
+    validates :name, presence: true
+    validates :quantity, presence: true
+    validates :quantity, length: {minimum: 1}
+    validates :year, presence: true
+    validates :season, presence: true
     
+    belongs_to :user
 end
