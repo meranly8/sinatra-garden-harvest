35f004f51a97ddf0c2190b7c087eb342b250a7b7
Michelle Ranly <meranly8@gmail.com>
2020-12-21 12:36:22 -0500

    Narrowed amount of image URLs

--- public/images/garden_images.rb
+++ public/images/garden_images.rb
@@ -1,7 +1,4 @@
 harvest1 = "https://i.imgur.com/AA5bavc.jpg"
-harvest2 = "https://i.imgur.com/b8lUeeV.jpeg"
-rainbow_harvest = "https://i.imgur.com/HczXbUXb.jpg"
-tomatoes = "https://i.imgur.com/L806Yavb.jpg"
 strawberries = "https://i.imgur.com/JSeKlqFb.jpg"
 kitty = "https://i.imgur.com/1Fj8lNE.jpeg"
 garden1 = "https://i.imgur.com/H7iFIUT.jpg"
